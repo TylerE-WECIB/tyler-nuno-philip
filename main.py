6d727e50f38d5c64bb566873ca95b38dd2bbf463
Nuno <61714135+NunoR1@users.noreply.github.com>
2024-04-16 13:59:24 -0400

    Titlke screen

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 
 def title_screen():
+    print("""
+  _________                                  .__  _________            .___         .___                   
+ /   _____/____    _____  __ ______________  |__| \_   ___ \  ____   __| _/____   __| _/______  _  ______  
+ \_____  \\__  \  /     \|  |  \_  __ \__  \ |  | /    \  \/ /  _ \ / __ |/ __ \ / __ |/  _ \ \/ \/ /     \ 
+ /        \/ __ \|  Y Y  \  |  /|  | \// __ \|  | \     \___(  <_> ) /_/ \  ___// /_/ (  <_> )     /   |  \\
+/_______  (____  /__|_|  /____/ |__|  (____  /__|  \______  /\____/\____ |\___  >____ |\____/ \/\_/|___|  /
+        \/     \/      \/                  \/             \/            \/    \/     \/                 \/ 
+Duel of Destiny: 
+CTI Ultimate Edition First Strike Second Half Gamma Omicron Tau Upsilon: 
+"Beat the grass and alert the snake", "The swing of a sword cannot cut the mist from the sky" 
+Version 3.12cti""")
     
+title_screen()
