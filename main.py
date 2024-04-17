7995e61c2dd1f2dc696f76718a94d011e58b6bf0
Nuno <61714135+NunoR1@users.noreply.github.com>
2024-04-17 12:08:36 -0400

    finished prototype title and game over screens

--- main.py
+++ main.py
@@ -1,15 +1,64 @@
+import keyboard
+import os
 
-def title_screen():
-    print("""
-  _________                                  .__  _________            .___         .___                   
- /   _____/____    _____  __ ______________  |__| \_   ___ \  ____   __| _/____   __| _/______  _  ______  
- \_____  \\__  \  /     \|  |  \_  __ \__  \ |  | /    \  \/ /  _ \ / __ |/ __ \ / __ |/  _ \ \/ \/ /     \ 
- /        \/ __ \|  Y Y  \  |  /|  | \// __ \|  | \     \___(  <_> ) /_/ \  ___// /_/ (  <_> )     /   |  \\
-/_______  (____  /__|_|  /____/ |__|  (____  /__|  \______  /\____/\____ |\___  >____ |\____/ \/\_/|___|  /
-        \/     \/      \/                  \/             \/            \/    \/     \/                 \/ 
+def title_screen(): # title screen code
+    
+    selections = "> Start\n  Help"  
+    
+    
+    while True: # constantly checks for keyboard interaction and changes variables accordingly
+        title = f"""  _________                                  .__ 
+ /   _____/____    _____  __ ______________  |__|
+ \_____  \\__  \  /     \|  |  \_  __ \__  \ |  | 
+ /        \/ __ \|  Y Y  \  |  /|  | \// __ \|  | 
+/_______  (____  /__|_|  /____/ |__|  (____  /__| 
+        \/     \/      \/                  \/             
+_________            .___         .___                   
+\_   ___ \  ____   __| _/____   __| _/______  _  ______  
+/    \  \/ /  _ \ / __ |/ __ \ / __ |/  _ \ \/ \/ /     \ 
+\     \___(  <_> ) /_/ \  ___// /_/ (  <_> )     /   |  \\
+ \______  /\____/\____ |\___  >____ |\____/ \/\_/|___|  /
+        \/            \/    \/     \/                 \/ 
+
 Duel of Destiny: 
 CTI Ultimate Edition First Strike Second Half Gamma Omicron Tau Upsilon: 
 "Beat the grass and alert the snake", "The swing of a sword cannot cut the mist from the sky" 
-Version 3.12cti""")
-    
-title_screen()
+Version 3.12cti
+{selections}
+"""
+        print(title)
+        os.system("cls")
+        if keyboard.is_pressed("down"):
+            selections = "  Start\n> Help"
+            continue
+        if keyboard.is_pressed("up"):
+            selections = "> Start\n  Help"
+            continue
+        if keyboard.is_pressed("enter") and selections == "> Start\n  Help":
+           print("gmae has started")
+           break     
+        if keyboard.is_pressed("enter") and selections == "  Start\n> Help":
+            break
+
+
+def game_over():
+    selections = "> Once Again            Quit"
+
+    while True:
+        game_over_screen = f"""
+game over
+{selections}
+"""    
+        if keyboard.is_pressed("left"):
+            selections = "> Once Again            Quit"
+            continue
+        if keyboard.is_pressed("right"):
+            selections = "  Once Again          > Quit"
+            continue
+        if keyboard.is_pressed("enter") and selections == "> Once Again            Quit":
+           print("new gmae has started")
+           break     
+        if keyboard.is_pressed("enter") and selections == "  Once Again          > Quit":
+            break
+
+game_over()
