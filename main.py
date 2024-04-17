45aadebccfd609020afefa3dfd72a9ce3e3d6962
pfthomas-1 <145382532+pfthomas-1@users.noreply.github.com>
2024-04-17 13:25:25 -0400

    Added Questions, PythonQuestions, and CtiQuestions classes

--- main.py
+++ main.py
@@ -172,3 +172,5 @@ question_25 = CtiQuestions("25",
                            "Destination MAC Address",
                            "Source MAC Address",
                            "Destination MAC Address")
+
+question_list = [question_1, question_2, question_3, question_4, question_5, question_6, question_7, question_8, question_9, question_10, question_11, question_12, question_13, question_14, question_15, question_16, question_17, question_18, question_19, question_20, question_21, question_22, question_23, question_24, question_25]
